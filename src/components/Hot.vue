a1bd4e2b6d11c81834012342dd37b091712e2a28
alicifes <635023574@qq.com>
2021-08-10 23:46:42 +0800

    hotpage

--- src/components/Hot.vue
+++ src/components/Hot.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="com-container">
-        <div class="com-chart" ref="hot_ref">
-        </div>
+        <div class="com-chart" ref="hot_ref"></div>
+        <span class="iconfont arr-left" @click="toLeft" :style="comStyle">&#xe6ef; </span>
+        <span class="iconfont arr-right" @click="toRight" :style="comStyle">&#xe6ed;</span>
+        <span class="cat-name" :style="comStyle">{{catName}}</span>
     </div>
 </template>
 <script>
@@ -10,7 +12,23 @@ export default {
     data(){
         return{
             chartInstance:null,
-            allData:null
+            allData:null,
+            currentIndex:0,    //当前展示的一级分类数据
+            titleFontSize:0
+        }
+    },
+    computed:{
+        catName(){
+            if(!this.allData){
+                return ''
+            }else{
+                return this.allData[this.currentIndex].name
+            }
+        },
+        comStyle(){
+            return{
+                fontSize:this.titleFontSize+'px'
+            }
         }
     },
     destroyed(){
@@ -23,11 +41,50 @@ export default {
     methods:{
         //初始化echarts的实例对象
         initChart(){
-            this.chartInstance = this.$echarts.init(this.$refs.hot_ref,'chalk') 
+            this.chartInstance = this.$echarts.init(this.$refs.hot_ref,'chalk')
             const initOption = {
+                title:{
+                    text:'▎热销商品占比',
+                    left:20,
+                    top:20
+                },
+                legend:{
+                    top:'15%',
+                    icon:'circle'
+                },tooltip:{
+                    // tooltip 工具提示
+                    show:true,
+                    formatter:arg=>{
+                        const thirdCategory = arg.data.children
+                        // 计算出所有三级分类数值的总和
+                        let total = 0
+                        thirdCategory.forEach(item=>{
+                            total += item.value
+                        })
+                        let retStr = ''
+                        thirdCategory.forEach(item=>{
+                            retStr +=`
+                            ${item.name}:${parseInt(item.value / total*100)+'%'}
+                            <br/>
+                            `
+                        })
+                        return retStr
+                    }
+                },
                 series:[
                     {
-                        type:'pie'
+                        type:'pie',
+                        label:{
+                            show:false
+                        },
+                        emphasis:{
+                            label:{
+                                show:true
+                            },
+                            labelLine:{
+                                show:false
+                            }
+                        }
                     }
                 ]
             }
@@ -42,13 +99,14 @@ export default {
         },
         updataChart(){
             // 处理图标所需要的数据
-            const legendData = this.allData[0].children.map(item=>{
+            const legendData = this.allData[this.currentIndex].children.map(item=>{
                 return item.name
             })
-            const seriesData = this.allData[0].children.map(item=>{
+            const seriesData = this.allData[this.currentIndex].children.map(item=>{
                 return{
                     name: item.name,
-                    value:item.value
+                    value:item.value,
+                    children:item.children
                 }
             })
             const dataOption = {
@@ -65,16 +123,73 @@ export default {
             this.chartInstance.setOption(dataOption)
         },
         screenAdapter(){
-            const adapterOption = {}
+            this.titleFontSize = this.$refs.hot_ref.offsetWidth/100*3.6
+            const adapterOption = {
+                title:{
+                    textStyle:{
+                        fontSize:this.titleFontSize,
+                        center:['50%','50%']
+                    }
+                },
+                // 设置图例大小
+                legend:{
+                    itemWidth:this.titleFontSize/2,
+                    itemHeight:this.titleFontSize/2,
+                    itemGap:this.titleFontSize/2,
+                    textStyle:{
+                        fontSize:this.titleFontSize/2
+                    }
+                },
+                series:{
+                    radius:this.titleFontSize*4.5
+                }
+            }
             this.chartInstance.setOption(adapterOption)
+            this.updataChart()
+        },
+        toLeft(){
+            this.currentIndex--    //更新当前的currentIndex值
+            if(this.currentIndex<0){
+                this.currentIndex = this.allData.length-1
+            }
+            this.updataChart()     //更新界面
+        },
+        toRight(){
+            this.currentIndex++
+            if(this.currentIndex > this.allData.length -1){
+                this.currentIndex =  0
+            }
+            this.updataChart()
         }
     }
 }
-</script> 
+</script>
 
 <style lang="less" scoped>
 .com-chart{
-    width: 1800px;
-    height: 900px;
+    width: 1920px;
+    height: 1080px;
+}
+.arr-left{
+    position: absolute;
+    left: 10%;
+    top:50%;
+    transform: translateY(-50%);
+    cursor: pointer;          //鼠标移动到上面会出现指示
+    color: white;
+}
+.arr-right{
+    position: absolute;
+    right: 10%;
+    top:50%;
+    transform: translateY(-50%);
+    cursor: pointer;
+    color: white;
+}
+.cat-name {
+    position: absolute;
+    left: 80%;
+    bottom: 20px;
+    color: white;
 }
 </style>
